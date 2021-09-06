e7aa190c019155d71d97b52111a1e8480df52e47
show333 <hiratashowtime@gmail.com>
2021-09-07 08:34:06 +0900

    add google.plist

--- TwoYears/ViewController/ChatRoomViewController.swift
+++ TwoYears/ViewController/ChatRoomViewController.swift
@@ -29,7 +29,7 @@ class ChatRoomViewController: UIViewController {
     var userTeamColor : String?
     
     let userMyBrands = UserDefaults.standard.string(forKey: "userBrands")
-
+    
     let uid = Auth.auth().currentUser?.uid
     let DB = Firestore.firestore().collection("Rooms").document("karano").collection("kokoniireru")
     
@@ -65,7 +65,7 @@ class ChatRoomViewController: UIViewController {
         } else if dragons?.teamname == "blue" || dragons?.teamname == "purple" {
             documentTeamColor = "violet"
         }
-                
+        
         if dragons?.userteamname == "red" || dragons?.userteamname == "yellow" {
             userTeamColor = "orange"
         } else if dragons?.userteamname == "blue" || dragons?.userteamname == "purple" {
@@ -73,7 +73,7 @@ class ChatRoomViewController: UIViewController {
         }
         
         UserDefaults.standard.set(dragons!.documentId, forKey: "documentId")
-
+        
         setupNotification()
         setupChatRoomTableView()
         setSwipeBack()
@@ -121,11 +121,11 @@ class ChatRoomViewController: UIViewController {
         chatRoomTableView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
         chatRoomTableView.scrollIndicatorInsets = .init(top: 0, left: 0, bottom: 0, right: 0)
     }
-
+    
     private func setupChatRoomTableView() {
         self.navigationItem.title = ("コメント数" + String(dragons!.messageCount))
-
-
+        
+        
         self.navigationItem.backBarButtonItem = UIBarButtonItem(
             title: "戻る",
             style: .plain,
@@ -139,46 +139,46 @@ class ChatRoomViewController: UIViewController {
         } else if dragons?.teamname == "yellow"{
             differentLabel.backgroundColor = #colorLiteral(red: 1, green: 0.8386732961, blue: 0.1333333403, alpha: 0.7974526849)
             differentLabel.text = "チームカラーが違うため、コメントできません."
-//            differentLabel.backgroundColor = .white
+            //            differentLabel.backgroundColor = .white
         } else if dragons?.teamname == "red"{
             differentLabel.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0.1150693222, alpha: 0.7047755282)
             differentLabel.text = "チームカラーが違うため、コメントできません."
-//            differentLabel.backgroundColor = .white
+            //            differentLabel.backgroundColor = .white
         } else if dragons?.teamname == "blue"{
             differentLabel.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 0.7977552817)
             differentLabel.text = "チームカラーが違うため、コメントできません."
-//            differentLabel.backgroundColor = .white
+            //            differentLabel.backgroundColor = .white
         } else if dragons?.teamname == "purple"{
             differentLabel.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.7983879842)
             differentLabel.text = "チームカラーが違うため、コメントできません."
-//            differentLabel.backgroundColor = .white
+            //            differentLabel.backgroundColor = .white
         } else if dragons?.teamname == "green" {
             differentLabel.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 0.9030126284)
             differentLabel.text = "チームカラーが違うため、コメントできません."
         }
-
+        
         
         chatRoomTableView.delegate = self
         chatRoomTableView.dataSource = self
         
         chatRoomTableView.keyboardDismissMode = .interactive
-//        chatRoomTableView.transform = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: 0)
+        //        chatRoomTableView.transform = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: 0)
         chatRoomTableView.register(UINib(nibName: "ChatRoomTableViewCell", bundle: nil), forCellReuseIdentifier: cellId)
         chatRoomTableView.backgroundColor = #colorLiteral(red: 0.9387103873, green: 0.8334191148, blue: 0.6862602769, alpha: 1)
         view.backgroundColor = #colorLiteral(red: 0.9387103873, green: 0.8334191148, blue: 0.6862602769, alpha: 1)
-//        #colorLiteral(red: 0.3137254902, green: 0.3137254902, blue: 0.3137254902, alpha: 1)
+        //        #colorLiteral(red: 0.3137254902, green: 0.3137254902, blue: 0.3137254902, alpha: 1)
         //        #colorLiteral(red: 0.9387103873, green: 0.8334191148, blue: 0.6862602769, alpha: 1)
         
     }
-
+    
     override var inputAccessoryView: UIView? {
         get {
             
             if documentTeamColor == userTeamColor || dragons?.userBrands == userMyBrands
-                    {
-                        return chatInputAccessoryView
-                    }else {
-                        return differentView
+            {
+                return chatInputAccessoryView
+            }else {
+                return differentView
             }
         }
     }
@@ -189,12 +189,10 @@ class ChatRoomViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = true
-//        self.navigationItem.title = ("コメント数" + String(rurubus.count))
+        //        self.navigationItem.title = ("コメント数" + String(rurubus.count))
     }
-
-//    キーボード下げる
-
-
+    
+    //    キーボード下げる
     
     private func goodmanmember() {
         let chatRoomDocId = dragons!.documentId
@@ -280,9 +278,9 @@ class ChatRoomViewController: UIViewController {
                         return m1Date < m2Date
                         
                     }
-                                        
+                    
                     self.chatRoomTableView.reloadData()
-//                    self.chatRoomTableView.scrollToRow(at: IndexPath(row: rurubus.count - 1, section: 0), at: .bottom, animated: true)
+                //                    self.chatRoomTableView.scrollToRow(at: IndexPath(row: rurubus.count - 1, section: 0), at: .bottom, animated: true)
                 case .modified, .removed:
                     print("ランダムUSERID","")
                     
@@ -304,11 +302,9 @@ extension ChatRoomViewController: ChatInputAccessoryViewDelegate{
     
     
     private func addMessageToFirestore(text: String) {
-        
-        
-        
+   
         chatInputAccessoryView.removeText()
-                
+        
         func randomString(length: Int) -> String {
             let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
             return String((0..<length).map{ _ in characters.randomElement()! })
@@ -336,7 +332,7 @@ extension ChatRoomViewController: ChatInputAccessoryViewDelegate{
                 "sendImageURL": "",
                 "teamname": userteamname,
             ] as [String: Any]
-    
+            
             DB.document(chatRoomDocId).collection("messages").document(commentId).setData(docData) { (err) in
                 if let err = err {
                     print("メッセージ情報の保存に失敗しました。ss\(err)")
@@ -356,7 +352,7 @@ extension ChatRoomViewController: ChatInputAccessoryViewDelegate{
                 
                 if querySnapshot!.documents.count == 0 {
                     
-
+                    
                     let commentId = uid+"comentId"
                     
                     comment(randomuserId: "",commentId: commentId)
@@ -364,9 +360,9 @@ extension ChatRoomViewController: ChatInputAccessoryViewDelegate{
                     DB.document(chatRoomDocId).collection("members").document(uid).setData(["randomUserId": randomUserId], merge: true)
                     
                     DB.document(chatRoomDocId).setData([uid: true], merge: true)
-
+                    
                     Firestore.firestore().collection("users").document(uid).setData(["The_earliest":true], merge: true)
-
+                    
                     
                 } else if querySnapshot!.documents.count == 1 {
                     
@@ -381,7 +377,7 @@ extension ChatRoomViewController: ChatInputAccessoryViewDelegate{
                             let comentId = randomString(length: 15)
                             comment(randomuserId: randomUserId, commentId: comentId)
                             DB.document(chatRoomDocId).collection("messages").document(uid+"comentId").updateData(["randomUserId":randomUserId])
-
+                            
                         }
                     }
                     
@@ -398,22 +394,22 @@ extension ChatRoomViewController: ChatInputAccessoryViewDelegate{
                 }
             }
             
-//            self.DB.document(chatRoomDocId).collection("messages").getDocuments() { (querySnapshot, err) in
-//                if let err = err {
-//                    print("Error getting documents: \(err)")
-//                } else {
-//                    print(querySnapshot!.documents.count)
-//                    self.DB.document(chatRoomDocId).updateData(["messagecount":querySnapshot!.documents.count as Int])
-
-//                }
-//            }
-//            "createdLatestAt": FieldValue.serverTimestamp(),
+            //            self.DB.document(chatRoomDocId).collection("messages").getDocuments() { (querySnapshot, err) in
+            //                if let err = err {
+            //                    print("Error getting documents: \(err)")
+            //                } else {
+            //                    print(querySnapshot!.documents.count)
+            //                    self.DB.document(chatRoomDocId).updateData(["messagecount":querySnapshot!.documents.count as Int])
+            
+            //                }
+            //            }
+            //            "createdLatestAt": FieldValue.serverTimestamp(),
             self.DB.document(chatRoomDocId).setData(["createdLatestAt": FieldValue.serverTimestamp()],merge: true)
             self.DB.document(chatRoomDocId).updateData(["messagecount": FieldValue.increment(Int64(1))])
         }
     }
 }
-    
+
 
 extension ChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
     
@@ -434,17 +430,17 @@ extension ChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = chatRoomTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatRoomTableViewCell
-//        cell.messageTextView.text = rurubus[indexPath.row]
-//        let nowtime : Date = Date()
-//        let nowtimemoment = moment(nowtime)
+        //        cell.messageTextView.text = rurubus[indexPath.row]
+        //        let nowtime : Date = Date()
+        //        let nowtimemoment = moment(nowtime)
         
         
         cell.sendImageView.image = nil
         cell.Imageheight.constant = 0
-
+        
         let comentjidate = rurubus[indexPath.row].createdTime.dateValue()
         let comentjimoment = moment(comentjidate)
-
+        
         let dateformatted1 = comentjimoment.format("hh:mm")
         let dateformatted2 = comentjimoment.format("MM/dd")
         cell.dateLabel.text = dateformatted1
@@ -505,7 +501,7 @@ extension ChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
         } else if rurubus[indexPath.row].userBrands == "TG5" {
             cell.userImage.image = UIImage(named:"TG5")!
         }
-
+        
         cell.userImage.layer.cornerRadius = 17.5
         
         
@@ -517,13 +513,9 @@ extension ChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
                 cell.Imageheight.constant = width*0.55
                 cell.backView.backgroundColor = .clear
             }
-
         }
-        
         return cell
-        
     }
-    
 }
 
 
