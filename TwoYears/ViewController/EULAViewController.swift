e907e584b3477cf32c9e69555cfc79f106f86452
show333 <hiratashowtime@gmail.com>
2021-12-30 13:36:08 +0900

    change only logo

--- TwoYears/ViewController/EULAViewController.swift
+++ TwoYears/ViewController/EULAViewController.swift
@@ -14,6 +14,7 @@ class EULAViewController: UIViewController {
     
     @IBAction func euTappedButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(1, forKey: "EULA")
     }
     
     override func viewDidLoad() {
