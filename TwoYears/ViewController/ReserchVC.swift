7a68c8254bb3471c98a2aa8c25ce04cff5ccc3b7
平田翔大 <hiratashoudai@hiratashoudainoMacBook-Air.local>
2022-01-18 14:35:32 +0900

    before pushnotification

--- TwoYears/ViewController/ReserchVC.swift
+++ TwoYears/ViewController/ReserchVC.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class ReserchVC:UIViewController{
     private let cellId = "cellId"
-
-    @IBOutlet weak var reserchTableView: UITableView!
+    let db = Firestore.firestore()
+    var selectBool:Bool = false
     
+    @IBOutlet weak var explainLabel: UILabel!
+    @IBOutlet weak var selectButton: UIButton!
+    
+    @IBAction func selectTappedButton(_ sender: Any) {
+        if selectBool == false {
+            selectBool = true
+            selectButton.setTitle("ID", for: .normal)
+            inputTextField.attributedPlaceholder = NSAttributedString(string: "ユーザーIDで検索", attributes: nil)
+            print("いっっぢぢ")
+        } else {
+            selectBool = false
+            selectButton.setTitle("Name", for: .normal)
+            inputTextField.attributedPlaceholder = NSAttributedString(string: "ユーザーネームで検索", attributes: nil)
+            print("あいう")
+        }
+    }
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var reserchTableView: UITableView!
+    @IBOutlet weak var reserchButton: UIButton!
+    @IBAction func reserchTappedButton(_ sender: Any) {
+        print(inputTextField.text)
+        getAccount()
+    }
+    
+    func getAccount(){
+        db.collection("users").whereField("この人のアドレス", isEqualTo: inputTextField.text ?? "")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                    }
+                }
+            }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 任意の処理
+        inputTextField.resignFirstResponder()
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        explainLabel.text = "←タップで条件変更"
+
+        
+        print("aaaaaaa",selectBool)
+        if selectBool == false {
+            selectButton.setTitle("Name", for: .normal)
+            inputTextField.attributedPlaceholder = NSAttributedString(string: "ユーザーネームで検索", attributes: nil)
+
+
+        } else {
+            selectButton.setTitle("ID", for: .normal)
+            inputTextField.attributedPlaceholder = NSAttributedString(string: "ユーザーIDで検索", attributes: nil)
+
+        }
         reserchTableView.dataSource = self
         reserchTableView.delegate = self
     }
---
+++ TwoYears/ViewController/adVC.swift
@@ -0,0 +1,12 @@
+//
+//  adVC.swift
+//  TOTALGOOD
+//
+//  Created by 平田翔大 on 2022/01/16.
+//
+
+import UIKit
+
+class adVC:UIViewController{
+    
+}
